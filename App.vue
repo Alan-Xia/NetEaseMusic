7dcd01a1f0db22875745a5a568b63361bb0a38c9
Alan-Xia <237490540@qq.com>
2021-06-27 23:31:31 +0800

    测试

--- App.vue
+++ App.vue
@@ -2,13 +2,13 @@
 	import '@/assets/iconfont.css'
 	export default {
 		onLaunch: function() {
-			console.log('App Launch')
+			
 		},
 		onShow: function() {
-			console.log('App Show')
+			
 		},
 		onHide: function() {
-			console.log('App Hide')
+			
 		}
 	}
 </script>
