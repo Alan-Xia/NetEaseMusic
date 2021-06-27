a3a82a337f97630e80d26b7beecb6848ca3bcf1f
Alan-Xia <237490540@qq.com>
2021-06-27 23:22:04 +0800

    搭建网易云音乐的uni-app项目

---
+++ pages/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="iconfont iconlike">项目</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
