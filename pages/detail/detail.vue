3e6d564bd1f1b89056ac5247f75f4f5a3cc937ff
Alan-Xia <237490540@qq.com>
2021-07-14 23:43:05 +0800

    处理歌曲详情页

--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -15,9 +15,53 @@
 						<text class="iconfont" :class="playicon"></text>
 					</view>
 				</view>
+	
 				<view class="detail-lyric">
 					<view class="detail-lyric-wrap">
 						<view class="detail-lyric-item"></view>
+					</view>
+				</view>
+	
+				<view class="detail-like">
+					<view class="detail-like-title">喜欢这首歌的人也听</view>
+					<view class="detail-like-list">
+						<view class="detail-like-item" v-for="(item,index) in songSimi" :key="index" @tap="handleToSimi(item.id)">
+							<view class="detail-like-img"><image :src="item.album.picUrl" mode=""></image></view>
+							<view class="detail-like-song">
+								<view>{{item.name}}</view>
+								<view>
+									<image v-if="item.privilege.flag > 60 && item.privilege.flag < 70" src="../../static/dujia.png" mode=""></image>
+									<image v-if="item.privilege.maxbr == 999000" src="../../static/sq.png" mode=""></image>
+									{{item.artists[0].name}} - {{item.name}}
+								</view>
+							</view>
+							<!--播放按钮-->
+							<text class="iconfont iconbofang"></text>
+						</view>
+					</view>
+				</view>
+				
+				<view class="detail-comment">
+					<view class="detail-comment-title">精彩评论</view>
+					<view class="detail-comment-item" v-for="(item,index) in songComment" :key="index">
+						<view class="detail-comment-img">
+							<!--评论用户头像-->
+							<image :src="item.user.avatarUrl" mode=""></image>
+						</view>
+						<view class="detail-comment-content">
+							<view class="detail-comment-head">
+								<view class="detail-comment-name">
+									<view>{{ item.user.nickname }}</view>
+									<view>{{ item.time | formatTime }}</view>
+								</view>
+								<view class="detail-comment-like">
+									{{ item.likedCount | formatCount }} <text class="iconfont iconlike"></text>
+								</view>
+							</view>
+							<view class="detail-comment-text">
+								{{ item.content }}
+							</view>
+						</view>
 					</view>
 				</view>
 			</scroll-view>
@@ -37,7 +81,7 @@
 				songSimi : [],
 				// 评论
 				songComment : [],
-				// 歌词
+				// 歌曲
 				songLyric : [],
 				// 歌词高亮
 				lyricIndex : 0,
@@ -60,16 +104,31 @@
 			})
 			this.getAllData(options.id)
 		},
+		filters: {
+			formatTime (value) {
+				// 定义一个时间数组
+					var date = new Date(value);
+					// 返回日期
+					return date.getFullYear() + '年' + (date.getMonth() + 1) + '月' + date.getDate() + '日';
+			}
+		},
 		methods: {
 			async getAllData (id) {
-				let [res1,res2] = await Promise.all([
+				let [res1,res2,res3,res4,res5] = await Promise.all([
 														request({url: `/song/detail?ids=${id}`}),
-														request({url: `/lyric?id=${id}`})
+														request({url: `/lyric?id=${id}`}),
+														request({url: `/song/url?id=${id}`}),
+														request({url: `/simi/song?id=${id}`}),
+														request({url: `/comment/music?id=${id}`})
 														])
 				this.songDetail = res1.data.songs[0]
 				let songLyric = res2.data.lrc.lyric
-				
+				this.songSimi = res4.data.songs
+				this.songComment = res5.data.hotComments
 				uni.hideToast()
+			},
+			handleToSimi (id) {
+				
 			}
 		}
 	}
