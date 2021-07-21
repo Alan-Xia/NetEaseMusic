ce91707e5c6730b37bea6b73cbf9a554acae17c9
Alan-Xia <237490540@qq.com>
2021-07-21 22:29:18 +0800

    新增首页骨架屏

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -7,7 +7,17 @@
 					<text class="iconfont iconsearch"></text>
 					<input type="text" value="" placeholder="搜索歌曲" />
 				</view>
-				<view class="index-list">
+				<view class="skeleton" v-if="loading">
+					<Skeleton
+					  :loading="loading"
+					  :avatarSize="skeleton1.avatarSize"
+					  :row="skeleton1.row"
+					  :showTitle="skeleton1.showTitle"
+						v-for="(item,key) in 4"
+						:key="key"
+					></Skeleton>
+				</view>
+				<view class="index-list" v-else>
 					<view class="index-list-item" v-for="(item,index) in topList" :key="index" @tap="handleToList(item.id)">
 						<view class="index-list-img">
 							<image :src="item.coverImgUrl" mode=""></image>
@@ -25,20 +35,35 @@
 
 <script>
 	import MusicHeader from '@/components/musichead/musichead.vue'
+	import Skeleton from '@/components/skeleton/skeleton.vue'
 	import request from '@/request/api'
 	export default {
 		data() {
 			return {
-				topList: []
+				topList: [],
+				loading: true,
+				skeleton1 : {
+				  avatarSize: '52px',
+				  row: 3,
+				  showTitle: true,
+				}
 			}
 		},
 		components: {
-			MusicHeader
+			MusicHeader,
+			Skeleton
 		},
 		onLoad() {
+			this.reloadData()
 			this.getTopList()
 		},
 		methods: {
+			reloadData() {
+			  this.loading = true
+			  setTimeout(() => {
+			    this.loading = false
+			  }, 2000)
+			},
 			async getTopList () {
 				const {data} = await request({url:'/toplist/detail'})
 				this.topList = data.list.slice(0,4)
